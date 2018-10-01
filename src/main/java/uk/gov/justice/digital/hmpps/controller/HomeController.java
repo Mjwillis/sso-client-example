418b4f97940ecf936261c3b589cdb54acb73657c
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-10-01 13:56:33 +0100

    simple sso client

---
+++ src/main/java/uk/gov/justice/digital/hmpps/controller/HomeController.java
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+    @GetMapping("/securedPage")
+    public String securedPage() {
+        return "securedPage";
+    }
+
+}
